e468da5ca0fa68206fbb4eb80125d994c384c403
Lars Hupel <lars.hupel@mytum.de>
2021-01-09 12:30:52 +0100

    debug

--- kind/test.sh
+++ kind/test.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-set -eo pipefail
+set -exo pipefail
 
 rm -f ./kind/actual
 
-for i in `seq 1 100`; do curl -s http://localhost/$i | jq .args.response; done > ./kind/actual
+for i in `seq 1 100`; do curl -s http://localhost/$i ; done > ./kind/actual
 
 diff ./kind/expected ./kind/actual
